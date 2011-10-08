2f3f42e7717ab79ab666ab09b78196728e776fef
Ludovic LEGENDART <ludovic@debianbox.(none)>
2011-10-08 18:02:05 +0200

    report excel file generation is now a class method

--- app/steering/models.py
+++ app/steering/models.py
@@ -195,6 +195,138 @@ class Report(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('project-detail', [str(self.project.slug), str(self.id)])
+    
+    def generate_excel_file(self):
+        wb = Workbook()
+        sheet_title = self.project.name + ' - Report'
+        sheet = wb.add_sheet(sheet_title)
+        # styles & fonts definitions
+        title = XFStyle()
+        font_title = Font()
+        font_title.name = 'Arial'
+        font_title.bold = True
+        font_title.height = 400
+        title.font = font_title
+        title_date = XFStyle()
+        title_date.num_format_str = 'DD-MM-YY'
+        title_date.font = font_title
+        subtitle = XFStyle()
+        font_subtitle = Font()
+        font_subtitle.name = 'Arial'
+        font_subtitle.bold = True
+        font_subtitle.height = 300
+        subtitle.font = font_subtitle
+        header = XFStyle()
+        font_header = Font()
+        font_header.name = 'Arial'
+        font_header.bold = True
+        font_header.height = 230
+        header.font = font_header
+        normal = XFStyle()
+        font_normal = Font()
+        font_normal.name = 'Arial'
+        font_normal.height = 190
+        normal.font = font_normal
+        normal_date = XFStyle()
+        normal_date.num_format_str = 'D/M/YY'
+        normal_date.font = font_normal
+        normal_datetime = XFStyle()
+        normal_datetime.num_format_str = 'D/M/YY h:mm'
+        normal_datetime.font = font_normal
+        # writing
+        sheet.write(0, 1, self.project.name + ' - Report:', title)
+        sheet.write(0, 2, self.date, title_date)
+        sheet.write(2, 0, 'Current iteration', subtitle)
+        sheet.write(4, 0, 'Name', header)
+        sheet.write(4, 1, 'URL', header)
+        sheet.write(4, 2, 'Provisional Start', header)
+        sheet.write(4, 3, 'Provisional End', header)
+        sheet.write(4, 4, 'Effective Start', header)
+        sheet.write(4, 5, 'Effective End', header)
+        sheet.write(4, 6, 'Total Subjects', header)
+        sheet.write(4, 7, 'Total Replies', header)
+        sheet.write(5, 0, self.current_iteration.name, normal)
+        sheet.write(5, 1, 'http://' + Site.objects.get_current().domain + str(self.current_iteration.get_absolute_url()), normal)
+        sheet.write(5, 2, self.current_iteration.provisional_start_date, normal_date)
+        sheet.write(5, 3, self.current_iteration.provisional_end_date, normal_date)
+        sheet.write(5, 4, self.current_iteration.effective_start_date, normal_date)
+        sheet.write(5, 5, self.current_iteration.effective_end_date, normal_date)
+        sheet.write(5, 6, self.current_iteration.total_subjects, normal)
+        sheet.write(5, 7, self.current_iteration.total_replies, normal)
+        ns = ast.literal_eval(self.new_subjects)
+        sheet.write(7, 0, 'New subjects (posted during the last 3 days)', subtitle)
+        sheet.write(7, 1, 'Total subjects: ' + str(ns['total']), subtitle)
+        sheet.write(7, 2, 'Total replies: ' + str(ns['replies']), subtitle)
+        sheet.write(9, 0, 'Name', header)
+        sheet.write(9, 1, 'Posted At', header)
+        sheet.write(9, 2, 'State', header)
+        sheet.write(9, 3, 'Total Replies', header)
+        line = 10
+        for s in ns['subjects']:
+            sheet.write(line, 0, s['name'], normal)
+            sheet.write(line, 1, s['posted_at'], normal_datetime)
+            sheet.write(line, 2, s['state'], normal)
+            sheet.write(line, 3, s['total_replies'], normal)
+            line += 1
+        line += 1
+        ops = ast.literal_eval(self.open_subjects)
+        sheet.write(line, 0, 'Open subjects', subtitle)
+        sheet.write(line, 1, 'Total subjects: ' + str(ops['total']), subtitle)
+        sheet.write(line, 2, 'Total replies: ' + str(ops['replies']), subtitle)
+        line += 2
+        sheet.write(line, 0, 'Name', header)
+        sheet.write(line, 1, 'Posted At', header)
+        sheet.write(line, 2, 'Total Replies', header)
+        line += 1
+        for s in ops['subjects']:
+            sheet.write(line, 0, s['name'], normal)
+            sheet.write(line, 1, s['posted_at'], normal_datetime)
+            sheet.write(line, 3, s['total_replies'], normal)
+            line += 1
+        line += 1
+        css = ast.literal_eval(self.closed_solved_subjects)
+        sheet.write(line, 0, 'Closed [Solved] subjects', subtitle)
+        sheet.write(line, 1, 'Total subjects: ' + str(css['total']), subtitle)
+        sheet.write(line, 2, 'Total replies: ' + str(css['replies']), subtitle)
+        line += 2
+        sheet.write(line, 0, 'Name', header)
+        sheet.write(line, 1, 'Posted At', header)
+        sheet.write(line, 2, 'Total Replies', header)
+        line += 1
+        for s in css['subjects']:
+            sheet.write(line, 0, s['name'], normal)
+            sheet.write(line, 1, s['posted_at'], normal_datetime)
+            sheet.write(line, 3, s['total_replies'], normal)
+            line += 1
+        line += 1
+        cus = ast.literal_eval(self.closed_unsolved_subjects)
+        sheet.write(line, 0, 'Closed [Unsolved] subjects', subtitle)
+        sheet.write(line, 1, 'Total subjects: ' + str(cus['total']), subtitle)
+        sheet.write(line, 2, 'Total replies: ' + str(cus['replies']), subtitle)
+        line += 2
+        sheet.write(line, 0, 'Name', header)
+        sheet.write(line, 1, 'Posted At', header)
+        sheet.write(line, 2, 'Total Replies', header)
+        line += 1
+        for s in cus['subjects']:
+            sheet.write(line, 0, s['name'], normal)
+            sheet.write(line, 1, s['posted_at'], normal_datetime)
+            sheet.write(line, 3, s['total_replies'], normal)
+            line += 1
+        line += 1
+        sheet.write(line, 0, 'Completed iterations', header)
+        for i in self.completed_iterations.all():
+            sheet.write(line, 1, i.name, normal)
+            line += 1
+        line += 1
+        sheet.write(line, 0, 'Upcoming iterations', header)
+        for i in self.upcoming_iterations.all():
+            sheet.write(line, 1, i.name, normal)
+            line += 1
+        wb.save(os.path.join(settings.BASE_DIR, 'tmp/report.xls'))
+#            f = open(os.path.join(settings.BASE_DIR, 'tmp/report.xls'), 'w')
+#            report_file = File(f)
+#            report.file.save(project.name + '_report_' + str(report.date) + '.xls', report_file, save=True)
 
     class Meta:
         verbose_name = u'Report'
@@ -319,138 +451,7 @@ def generate_report(project):
                 if iter.rank < report.current_iteration.rank and iter.state == State.objects.get(name='Finished'):
                     report.completed_iterations.add(iter)
             report.save()
-            wb = Workbook()
-            sheet = wb.add_sheet('Report')
-            style = XFStyle()
-            font = Font()
-            font.name = 'Arial'
-            font.bold = True
-            font.height = 300
-            style.font = font
-            sheet.write(0, 1, project.name + ' - Report - ' + str(report.date), style)
-            font.height = 25
-            style.font = font
-            sheet.write(2, 0, 'Current iteration', style)
-            font.height = 200
-            style.font = font
-            sheet.write(3, 0, 'Name', style)
-            sheet.write(3, 1, 'URL', style)
-            sheet.write(3, 2, 'Provisional Start', style)
-            sheet.write(3, 3, 'Provisional End', style)
-            sheet.write(3, 4, 'Effective Start', style)
-            sheet.write(3, 5, 'Effective End', style)
-            sheet.write(3, 6, 'Total Subjects', style)
-            sheet.write(3, 7, 'Total Replies', style)
-            font.height = 140
-            style.font = font
-            sheet.write(4, 0, report.current_iteration.name, style)
-            sheet.write(4, 1, 'http://' + Site.objects.get_current().domain + str(report.current_iteration.get_absolute_url()), style)
-            sheet.write(4, 2, report.current_iteration.provisional_start_date, style)
-            sheet.write(4, 3, report.current_iteration.provisional_end_date, style)
-            sheet.write(4, 4, report.current_iteration.effective_start_date, style)
-            sheet.write(4, 5, report.current_iteration.effective_end_date, style)
-            sheet.write(4, 6, report.current_iteration.total_subjects, style)
-            sheet.write(4, 7, report.current_iteration.total_replies, style)
-            font.height = 250
-            style.font = font
-            ns = ast.literal_eval(report.new_subjects)
-            sheet.write(6, 0, 'New subjects (posted during the last 3 days)', style)
-            sheet.write(6, 1, 'Total subjects: ' + str(ns['total']), style)
-            sheet.write(6, 2, 'Total replies: ' + str(ns['replies']), style)
-            font.height = 200
-            style.font = font
-            sheet.write(7, 0, 'Name', style)
-            sheet.write(7, 1, 'Posted At', style)
-            sheet.write(7, 2, 'State', style)
-            sheet.write(7, 3, 'Total Replies', style)
-            font.height = 140
-            style.font = font
-            line = 8
-            for s in ns['subjects']:
-                sheet.write(line, 0, s['name'], style)
-                sheet.write(line, 1, s['posted_at'], style)
-                sheet.write(line, 2, s['state'], style)
-                sheet.write(line, 3, s['total_replies'], style)
-                line += 1
-            font.height = 250
-            style.font = font
-            line += 1
-            ops = ast.literal_eval(report.open_subjects)
-            sheet.write(line, 0, 'Open subjects', style)
-            sheet.write(line, 1, 'Total subjects: ' + str(ops['total']), style)
-            sheet.write(line, 2, 'Total replies: ' + str(ops['replies']), style)
-            font.height = 200
-            style.font = font
-            line += 1
-            sheet.write(line, 0, 'Name', style)
-            sheet.write(line, 1, 'Posted At', style)
-            sheet.write(line, 2, 'Total Replies', style)
-            font.height = 140
-            style.font = font
-            line += 1
-            for s in ops['subjects']:
-                sheet.write(line, 0, s['name'], style)
-                sheet.write(line, 1, s['posted_at'], style)
-                sheet.write(line, 3, s['total_replies'], style)
-                line += 1
-            font.height = 250
-            style.font = font
-            line += 1
-            css = ast.literal_eval(report.closed_solved_subjects)
-            sheet.write(line, 0, 'Closed [Solved] subjects', style)
-            sheet.write(line, 1, 'Total subjects: ' + str(css['total']), style)
-            sheet.write(line, 2, 'Total replies: ' + str(css['replies']), style)
-            font.height = 200
-            style.font = font
-            line += 1
-            sheet.write(line, 0, 'Name', style)
-            sheet.write(line, 1, 'Posted At', style)
-            sheet.write(line, 2, 'Total Replies', style)
-            font.height = 140
-            style.font = font
-            line += 1
-            for s in css['subjects']:
-                sheet.write(line, 0, s['name'], style)
-                sheet.write(line, 1, s['posted_at'], style)
-                sheet.write(line, 3, s['total_replies'], style)
-                line += 1
-            font.height = 250
-            style.font = font
-            line += 1
-            cus = ast.literal_eval(report.closed_unsolved_subjects)
-            sheet.write(line, 0, 'Closed [Unsolved] subjects', style)
-            sheet.write(line, 1, 'Total subjects: ' + str(cus['total']), style)
-            sheet.write(line, 2, 'Total replies: ' + str(cus['replies']), style)
-            font.height = 200
-            style.font = font
-            line += 1
-            sheet.write(line, 0, 'Name', style)
-            sheet.write(line, 1, 'Posted At', style)
-            sheet.write(line, 2, 'Total Replies', style)
-            font.height = 140
-            style.font = font
-            line += 1
-            for s in cus['subjects']:
-                sheet.write(line, 0, s['name'], style)
-                sheet.write(line, 1, s['posted_at'], style)
-                sheet.write(line, 3, s['total_replies'], style)
-                line += 1
-            font.height = 250
-            style.font = font
-            line += 1
-            sheet.write(line, 0, 'Completed iterations', style)
-            for i in report.completed_iterations.all():
-                sheet.write(line, 1, i.name, style)
-                line += 1
-            line += 1
-            sheet.write(line, 0, 'Upcoming iterations', style)
-            for i in report.upcoming_iterations.all():
-                sheet.write(line, 1, i.name, style)
-                line += 1
-            wb.save(os.path.join(settings.BASE_DIR, 'tmp/report.xls'))
-#            f = open(os.path.join(settings.BASE_DIR, 'tmp/report.xls'), 'w')
-#            report_file = File(f)
-#            report.file.save(project.name + '_report_' + str(report.date) + '.xls', report_file, save=True)
+            report.generate_excel_file()
         except Iteration.DoesNotExist:
             pass
         
